2252e207c742dd2718fad120ea2243d8288a70a5
Sumeetsinh Ketankumar Rana <sumeetsinh28@gmail.com>
2024-07-24 23:47:20 +0400

    feat(notification): server functions

---
+++ server/notification-service/src/server.ts
@@ -0,0 +1,38 @@
+import "express-async-errors";
+import { Logger } from "winston";
+import { winstonLogger } from "gig-globe-helper-library";
+import { Application } from "express";
+import http from "http";
+
+import { config } from "./config";
+
+const SERVER_PORT = 4001;
+const log: Logger = winstonLogger(
+  `${config.ELASTIC_SEARCH_URL}`,
+  "notificationServer",
+  "debug"
+);
+
+export function start(app: Application): void {
+  startServer(app);
+  startQueues();
+  startElasticSearch();
+}
+
+async function startQueues(): Promise<void> {}
+
+function startElasticSearch(): void {}
+
+function startServer(app: Application): void {
+  try {
+    const httpServer: http.Server = new http.Server(app);
+    log.info(
+      `Worker with process id of ${process.pid} on notification server has started`
+    );
+    httpServer.listen(SERVER_PORT, () => {
+      log.info(`Notification server running on port ${SERVER_PORT}`);
+    });
+  } catch (error) {
+    log.log("error", "NotificationService startServer() method", error);
+  }
+}
