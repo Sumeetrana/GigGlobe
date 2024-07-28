17ef7427906f1a12634864ef69831e56a8f2179c
Sumeetsinh Ketankumar Rana <sumeetsinh28@gmail.com>
2024-07-28 18:54:59 +0400

    feat(gateway): routes config and health routes added

--- server/api-gateway-service/src/server.ts
+++ server/api-gateway-service/src/server.ts
@@ -19,8 +19,10 @@ import cors from "cors";
 import compression from "compression";
 import { StatusCodes } from "http-status-codes";
 import http from "http";
+
 import { config } from "@gateway/config";
 import { elasticSearch } from "@gateway/elasticsearch";
+import { appRoutes } from "@gateway/routes";
 
 const SERVER_PORT = 4000;
 const log: Logger = winstonLogger(
@@ -73,7 +75,9 @@ export class GatewayServer {
     app.use(urlencoded({ extended: true, limit: "200mb" }));
   }
 
-  private routesMiddleware(_app: Application): void {}
+  private routesMiddleware(app: Application): void {
+    appRoutes(app);
+  }
 
   private startElasticSearch(): void {
     elasticSearch.checkConnection();
