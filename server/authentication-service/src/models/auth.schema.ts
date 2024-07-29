b9f9092cce1e44acb12eaf73f0479ee022cc3ab7
Sumeetsinh Ketankumar Rana <sumeetsinh28@gmail.com>
2024-07-30 00:25:47 +0400

    feat(auth): adding indexes in email and username field

--- server/authentication-service/src/models/auth.schema.ts
+++ server/authentication-service/src/models/auth.schema.ts
@@ -8,51 +8,66 @@ type AuthUserCreationAttributes = Optional<
 >;
 
 const AuthModel: ModelDefined<IAuthDocument, AuthUserCreationAttributes> =
-  sequelize.define("auths", {
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "auths",
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      profilePublicId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      country: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      profilePicture: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      emailVerificationToken: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      emailVerified: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: Date.now(),
+      },
+      passwordResetToken: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      passwordResetExpires: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: new Date(),
+      },
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    profilePublicId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    country: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    profilePicture: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    emailVerificationToken: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    emailVerified: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: 0,
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      defaultValue: Date.now(),
-    },
-    passwordResetToken: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    passwordResetExpires: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: new Date(),
-    },
-  });
+    {
+      indexes: [
+        {
+          unique: true,
+          fields: ["email"],
+        },
+        {
+          unique: true,
+          fields: ["username"],
+        },
+      ],
+    }
+  );
